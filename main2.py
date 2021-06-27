c001add730fc9c59f9f7ff00b77b357d85046906
Adarsh <limit6@protonmail.com>
2021-06-27 19:21:07 +0530

    update main.py

--- main2.py
+++ main2.py
@@ -1,3 +1,4 @@
+import nltk
 from nltk.corpus import stopwords
 from nltk.cluster.util import cosine_distance
 import numpy as np
@@ -33,12 +34,6 @@ def sentence_similarity(sent1, sent2, stopwords=None):
         if w in stopwords:
             continue
         vector1[all_words.index(w)] += 1
-
-    # build the vector for the first sentence
-    for w in sent1:
-        if w in stopwords:
-            continue
-        vector1[all_words.index(w)] += 1
  
     # build the vector for the second sentence
     for w in sent2:
@@ -62,8 +57,29 @@ def build_similarity_matrix(sentences, stop_words):
 
 
 def generate_summary(file_name, top_n=5):
+    nltk.download("stopwords")
     stop_words = stopwords.words('english')
     summarize_text = []
 
     # Step 1 - Read text anc split it
     sentences =  read_article(file_name)
+
+    # Step 2 - Generate Similary Martix across sentences
+    sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+
+    # Step 3 - Rank sentences in similarity martix
+    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+    scores = nx.pagerank(sentence_similarity_graph)
+
+    # Step 4 - Sort the rank and pick top sentences
+    ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
+    print("Indexes of top ranked_sentence order are ", ranked_sentence)    
+
+    for i in range(top_n):
+      summarize_text.append(" ".join(ranked_sentence[i][1]))
+
+    # Step 5 - Offcourse, output the summarize texr
+    print("Summarize Text: \n", ". ".join(summarize_text))
+
+# let's begin
+generate_summary( "privacypolicy", 2)
